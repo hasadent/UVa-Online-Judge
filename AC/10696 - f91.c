cd9b97c5eee711c825ee133461c8fa1f47bcb53f
hasadent <hasadent@users.noreply.github.com>
2014-01-01 20:54:00 +0800

    Update 10696 - f91.c

--- AC/10696 - f91.c
+++ AC/10696 - f91.c
@@ -8,12 +8,17 @@ int main()
 {
     int N;
 
-    while (scanf("%d", &N) != EOF && N != 0)
+    scanf("%d", &N);
+    while (N != 0)
     {
-        if (N > 100)
-            printf("f91(%d) = %d\n", N, N-10);
+        if (N <= 100)
+            printf("f91(%d) = 91\n", N);
         else
-            printf("f91(%d) = %d\n", N, 91);
+        {
+            printf("f91(%d) = %d\n", N, N-10);
+        }
+
+        scanf("%d", &N);
     }
 
 #ifndef ONLINE_JUDGE
