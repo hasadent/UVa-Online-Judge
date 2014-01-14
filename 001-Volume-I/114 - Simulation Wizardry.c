c1f6e68debd15bc9654addfeaa1b030d488efa66
hasadent <hasadent@users.noreply.github.com>
2014-01-14 23:53:09 +0800

    Create 114 - Simulation Wizardry.c

---
+++ 001-Volume-I/114 - Simulation Wizardry.c
@@ -0,0 +1,103 @@
+/****
+ * 114 - Simulation Wizardry
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_G 52
+#define MAX_B 49*49
+
+int grid[MAX_G][MAX_G];
+int n, m;
+
+typedef struct bumper
+{
+    int v;
+    int c;
+} bumper;
+
+bumper bumpers[MAX_B];
+int b;
+
+#define RIGHT 0
+#define LEFT  2
+#define UP    1
+#define DOWN  3
+
+int next_direction[4] = {   DOWN,  RIGHT,     UP,   LEFT };
+/*                      | RIGHT |    UP |  LEFT |  DOWN  */
+int x_move[4]         = {      1,      0,     -1,      0 };
+int y_move[4]         = {      0,      1,      0,     -1 };
+
+int main()
+{
+    int wall_cost;
+    int direction;
+    int lifetime;
+
+    int total_value;
+    int value;
+
+    int i, j;
+    int x, y;
+    int new_x, new_y;
+
+
+    int hit_wall;
+    int bx;
+
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
+#endif
+    memset(grid, 0, sizeof(grid));
+
+    scanf("%d %d", &m, &n);
+    scanf("%d", &wall_cost);
+    scanf("%d", &b);
+
+    for (i = 1; i <= b; i+=1)
+    {
+        scanf("%d %d %d %d", &x, &y, &bumpers[i].v, &bumpers[i].c);
+        grid[x][y] = i;
+    }
+
+    total_value = 0;
+    while (scanf("%d %d %d %d", &x, &y, &direction, &lifetime) != EOF)
+    {
+        value = 0;
+        while (--lifetime > 0)
+        {
+            new_x = x + x_move[direction];
+            new_y = y + y_move[direction];
+
+            if (new_x == 1 || new_x == m || new_y == 1 || new_y == n)
+            {
+                lifetime -= wall_cost;
+                direction = next_direction[direction];
+                continue;
+            }
+
+            if ((bx = grid[new_x][new_y]) != 0)
+            {
+                lifetime -= bumpers[bx].c;
+                value    += bumpers[bx].v;
+                direction = next_direction[direction];
+                continue;
+            }
+
+            x = new_x;
+            y = new_y;
+        } /* while (lifetime >= 0) */
+
+        printf("%d\n", value);
+        total_value += value;
+
+    } /* while scanf */
+    printf("%d\n", total_value);
+
+#ifndef ONLINE_JUDGE
+    system("pause");
+#endif
+  return 0;
+}
