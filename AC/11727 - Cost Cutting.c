3d1ac0b266136bd523c6d541c18c676f0e106dd5
hasadent <hasadent@users.noreply.github.com>
2014-01-18 11:47:53 +0800

    Update 11727 - Cost Cutting.c

--- AC/11727 - Cost Cutting.c
+++ AC/11727 - Cost Cutting.c
@@ -2,23 +2,22 @@
  * 11727 - Cost Cutting
  */
 #include <stdio.h>
+#define MAX2(_a, _b)     (_a > _b ? _a : _b)
+#define MAX3(_a, _b, _c) MAX2(_a, MAX2(_b, _c))
+#define MIN2(_a, _b)     (_a < _b ? _a : _b)
+#define MIN3(_a, _b, _c) MIN2(_a, MIN2(_b, _c))
 
 int main()
 {
-    int t;
-    int a, b, c;
+    int t, i;
+    int a,b,c;
 
     scanf("%d", &t);
-    while (t-- > 0)
+    for (i = 1; i <= t; i++)
     {
         scanf("%d %d %d", &a, &b, &c);
-        if (a > b) 
-            printf("%d\n", b > c ? b : c);
-        else
-            printf("%d\n", a > c ? a : c);        
+        printf("Case %d: %d\n", i, a + b + c - MAX3(a,b,c) - MIN3(a,b,c));
     }
 
-
     return 0;
-
 }
