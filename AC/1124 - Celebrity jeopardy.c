b47de4e5f97818e7f3a788817649bb4aadde3d70
hasadent <hasadent@users.noreply.github.com>
2014-01-20 00:30:37 +0800

    Create 1124 - Celebrity jeopardy.c

---
+++ AC/1124 - Celebrity jeopardy.c
@@ -0,0 +1,14 @@
+/****
+ * 1124 - Celebrity jeopardy
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char buf[1024];
+    while (gets(buf) != NULL)
+        puts(buf);
+
+    return 0;
+}
