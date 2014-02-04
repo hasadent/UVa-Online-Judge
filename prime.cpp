2f75805e2b2d9928ea0cad95f59d27651299ce33
hasadent <hasadent@users.noreply.github.com>
2014-02-04 10:58:06 +0800

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,11 +1,12 @@
-const int PRIME_MAX = 1000001
+const int PRIME_MAX = 1000001;
+const int PRIME_MAX_SQRT = 256;
 int prime[PRIME_MAX];
 
 
 memset(prime, 0, sizeof(prime));
 prime[0] = prime[1] = 1;
 
-for (int i = 2; i < PRIME_MAX; i++)
+for (int i = 2; i < PRIME_MAX_SQRT; i++)
   if (prime[i] == 0)
      for (int j = i*2; j < PRIME_MAX; j += i)
        prime[j] = 1;
